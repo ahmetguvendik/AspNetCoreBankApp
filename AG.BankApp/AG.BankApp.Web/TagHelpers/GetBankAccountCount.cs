d65d241b9f9abfc65c321363e60e793ba116e50d
Ahmet Güvendik <ahmetguvendik01@gmail.com>
2022-04-08 18:07:23 +0300

    AspNetCoreBankApp

---
+++ AG.BankApp/AG.BankApp.Web/TagHelpers/GetBankAccountCount.cs
@@ -0,0 +1,24 @@
+﻿using AG.BankApp.Web.Data.Context;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace AG.BankApp.Web.TagHelpers
+{
+    [HtmlTargetElement("getAccountCount")]
+    public class GetBankAccountCount : TagHelper
+    {
+        public int ApplicationUserId { get; set; }
+        private readonly BankContext _context;
+        public GetBankAccountCount(BankContext context)
+        {
+            _context = context;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var accountCount = _context.Accounts.Count(x => x.ApplicationUserId == ApplicationUserId);
+            string html = $"<span>{accountCount}</span>";
+            
+            output.Content.SetHtmlContent(html);
+        }
+    }
+}
